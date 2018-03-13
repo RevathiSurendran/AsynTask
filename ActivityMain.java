3d83ee1dd7a8e853dcec8aaa5d7f8fe822e570f8
RevathiSurendran <36188592+RevathiSurendran@users.noreply.github.com>
2018-03-13 13:25:55 +0530

    asyn task demo

---
+++ ActivityMain.java
@@ -0,0 +1,86 @@
+package com.example.mca.asyntask;
+
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity
+{
+    ProgressDialog mProgressDialog;
+    ImageView image;
+    Button startButton;
+    EditText editText;
+
+    private static  String IMAGE_URL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=(EditText) findViewById(R.id.ed1);
+        image = (ImageView)findViewById(R.id.im1);
+        startButton = (Button)findViewById(R.id.b2);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IMAGE_URL=editText.getText().toString();
+                new DownloadImage().execute(IMAGE_URL);
+
+            }
+        });
+    }
+
+    private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Create a progressdialog
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            // Set progressdialog title
+            mProgressDialog.setTitle("Download Image Tutorial");
+            // Set progressdialog message
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            // Show progressdialog
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... URL) {
+
+            String imageURL = URL[0];
+
+            Bitmap bitmap = null;
+            try {
+                // Download Image from URL
+                InputStream input = new java.net.URL(imageURL).openStream();
+                // Decode Bitmap
+                bitmap = BitmapFactory.decodeStream(input);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            // Set the bitmap into ImageView
+            image.setImageBitmap(result);
+            // Close progressdialog
+            mProgressDialog.dismiss();
+        }
+    }
+}
+
+
